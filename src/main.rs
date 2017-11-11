3b99a65c7aa8ba47156a6a58d87204a02b401ba7
Audun Halland <audun.halland@gmail.com>
2017-11-11 12:26:36 +0100

    Set up one simple endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
+mod api;
+
 extern crate rocket;
-
-#[get("/")]
-fn hello() -> String {
-    format!("Hello, world")
-}
-
-#[get("/<name>")]
-fn hello_name(name: String) -> String {
-    format!("Hello, {}", name)
-}
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![hello, hello_name])
+        .mount("/", routes![api::index])
         .launch();
 }
